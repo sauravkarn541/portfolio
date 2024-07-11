c563f8e5cabe167db7242bb42842f1663abeb35f
Saurav Lal Karn <sauravkarn541@gmail.com>
2024-07-11 22:33:22 +0545

    Work started on About section

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <section id='about'>
       <div className='grid lg:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4'>
-        <div className='h-10'>About is here now</div>
+        <div className='h-10'>About is here</div>
       </div>
     </section>
   )
