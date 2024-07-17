96990834e29039b068319e300556cf16082c27e8
Saurav Lal Karn <sauravkarn541@gmail.com>
2024-07-17 22:32:51 +0545

    Animations added

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,49 @@ import Introduction from "../components/Introduction";
 import ThemeSwitch from "../components/ThemeSwitch";
 import { FloatingNav } from "../components/ui/FloatingNavbar";
 import About from "../components/About";
+import { motion } from "framer-motion";
+
+type SectionProps = {
+  id: string;
+  className: string;
+  children: React.ReactNode
+  type: string;
+}
+
+const Section = ({ id, className, children, type }:SectionProps) => (
+  <section
+    id={id}
+    className={type !== 'introduction' ? ` h-screen flex items-center justify-center ${className}` : ''}
+  >
+    {children}
+  </section>
+);
 
 export default function Home() {
   return (
     <main>
-      <FloatingNav navItems={[
-              {name: 'Home', link: '/', icon: <FaHome />},
-              {name: 'About', link: '#about'},
-              {name: 'Projects', link: '#projects'},
-              {name: 'Contact', link: '#contact'},
+      <FloatingNav
+        navItems={[
+          {name: 'Home', link: '/', icon: <FaHome />},
+          {name: 'About', link: '#about'},
+          {name: 'Projects', link: '#projects'},
+          {name: 'Contact', link: '#contact'},
         ]}
-          className="px-5 py-5 rounded-2xl"
-        />
+        className="px-5 py-5 rounded-2xl"
+      />
       <ThemeSwitch />
-      <Introduction />
-      <div className="max-w-8xl w-full">
-        <About />
-      </div>
+      <Section id="home" className="" type="introduction">
+        <Introduction />
+      </Section>
+      <Section id="about" className="bg-red-500" type="about">
+        About is here
+      </Section>
+      <Section id="projects" className="bg-blue-500" type="projects">
+        Projects is here
+      </Section>
+      <Section id="contact" className="bg-green-500" type="contact">
+        Contact is here
+      </Section>
     </main>
   );
 }
--- components/Introduction.tsx
+++ components/Introduction.tsx
@@ -6,28 +6,26 @@ import { FaLocationArrow } from 'react-icons/fa6'
 
 const Introduction = () => {
   return (
-    <section id='home'>
-      <AuroraBackground>
-        <div className='h-screen w-full relative flex items-center justify-center'>
-          <div className="grid col-span-1 px-4">
-            <div className="flex justify-center relative my-20 z-10">
-              <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
-                <TextGenerateEffect
-                  className="text-center text-[40px] md:text-5xl lg:text-6xl"
-                  words="Crafting Robust Full Stack Solutions" />
+    <AuroraBackground>
+      <div className='h-screen w-full relative flex items-center justify-center'>
+        <div className="grid col-span-1 px-4">
+          <div className="flex justify-center relative my-20 z-10">
+            <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
+              <TextGenerateEffect
+                className="text-center text-[40px] md:text-5xl lg:text-6xl"
+                words="Crafting Robust Full Stack Solutions" />
 
-                <p className="text-center md:tracking-wider mb-4 mt-2 text-sm md:text-lg lg:text-2xl dark:text-white text-black">
-                  Hi, I&apos;m Saurav, a Full Stack Developer based in Nepal.
-                </p>
-                <a href="#">
-                  <ShimmerButton title="Show my work"  icon={<FaLocationArrow />} position="right"/>
-                </a>
-              </div>
+              <p className="text-center md:tracking-wider mb-4 mt-2 text-sm md:text-lg lg:text-2xl dark:text-white text-black">
+                Hi, I&apos;m Saurav, a Full Stack Developer based in Nepal.
+              </p>
+              <a href="#">
+                <ShimmerButton title="Show my work"  icon={<FaLocationArrow />} position="right"/>
+              </a>
             </div>
           </div>
         </div>
-      </AuroraBackground>
-    </section>
+      </div>
+    </AuroraBackground>
   )
 }
 
